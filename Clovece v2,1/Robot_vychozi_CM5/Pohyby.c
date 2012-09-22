7748bfd4201d323079d9cb3aaad069a51f69542c
kysel <jkyzlink@gmail.com>
2012-09-22 22:16:29 +0200

    Revert "del robotika"
    This reverts commit 3319c76ec816eb64cea34d921fad9cf5c40ab3b3.

---
+++ Clovece v2,1/Robot_vychozi_CM5/Pohyby.c
@@ -0,0 +1,6 @@
+/*
+ * Pohyby.c
+ *
+ * Created: 22.9.2012 20:11:26
+ *  Author: jirka1
+ */ 
---
+++ Clovece v2,1/Robot_vychozi_CM5/Pohyby_clovece.h
@@ -0,0 +1,16 @@
+/*
+ * Pohyby_clovece.h
+ *
+ * Created: 22.9.2012 20:10:37
+ *  Author: jirka1
+ */ 
+
+
+#ifndef POHYBY_CLOVECE_H_
+#define POHYBY_CLOVECE_H_
+
+
+
+
+
+#endif /* POHYBY_CLOVECE_H_ */
