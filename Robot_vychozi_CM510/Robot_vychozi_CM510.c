de52ce4eeee8281a1cb30069f1e6ad003e2a8cd0
kysel <jkyzlink@gmail.com>
2012-10-03 21:13:45 +0200

    pokusy s cm510

--- Robot_vychozi_CM510/Robot_vychozi_CM510.c
+++ Robot_vychozi_CM510/Robot_vychozi_CM510.c
@@ -3,19 +3,30 @@
 
 void run()
 {
-    while(1)
-    {
-		for (int i=1; i!=5; i++)
-		{
-			motor[i].position(512);
-			_delay_ms(200);
-		}
-		
-		for (int i=1; i!=6; i++)
-		{
-			motor[i].position(400);
-			_delay_ms(200);
-		}
-					    
+	for (int i=1; i!=5; i++)
+	{
+		motor[i].speed(500);
 	}
+	
+		while(1)
+		{
+			for (int i=1; i!=5; i++)
+			{
+				motor[i].position(512);
+				_delay_ms(200);
+				while (motor[i].present_speed()>1)
+				{
+				}
+			}			
+			
+			for (int i=1; i!=6; i++)
+			{
+				motor[i].position(400);
+				_delay_ms(200);
+				while (motor[i].present_speed()>1)
+				{
+				}
+			}
+			
+		}
 }
