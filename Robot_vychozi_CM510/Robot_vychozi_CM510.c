59e32e75177167f0e6f69ab6ce8b1c9f9515d028
kysel <jkyzlink@gmail.com>
2012-10-03 16:28:19 +0200

    cm510 example

--- Robot_vychozi_CM510/Robot_vychozi_CM510.c
+++ Robot_vychozi_CM510/Robot_vychozi_CM510.c
@@ -1,8 +1,21 @@
 #include "bioloid_v2.1_cm510.h"
+#include <avr/delay.h>
+
 void run()
 {
     while(1)
-    	{
-				    
+    {
+		for (int i=1; i!=5; i++)
+		{
+			motor[i].position(512);
+			_delay_ms(200);
+		}
+		
+		for (int i=1; i!=6; i++)
+		{
+			motor[i].position(400);
+			_delay_ms(200);
+		}
+					    
 	}
 }
