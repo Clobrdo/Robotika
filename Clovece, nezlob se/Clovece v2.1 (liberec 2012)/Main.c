19bb5f401b89d1adc31f3a5f80acb1d776beb23e
kysel <jkyzlink@gmail.com>
2012-10-22 17:33:34 +0200

    podprogramy
    uchop, poloz, zvedni, povol, ...

--- Clovece, nezlob se/Clovece v2.1 (liberec 2012)/Main.c
+++ Clovece, nezlob se/Clovece v2.1 (liberec 2012)/Main.c
@@ -1,57 +1,53 @@
 /*
  * Pohyby.c
  *
- * Created: 22.9.2012 20:11:26
- *  Author: jirka1
- */ 
-
+ */
 #include "bioloid_v2.1.h"
 #include "pohyby.h"
 #include <avr/delay.h>
 
-/*  Pøíkazy knihovny:
+/*  P??kazy knihovny:
 
-	zakladni_pozice	()
-	nastav_rychlost	()
-	pozice			(è. pozice)
-	cekej			()			//ceka dokud se motory hybou
-	*uchop			()
-	*polož			()
-	
+	zakladni_pozice		()
+	nastav_rychlost		()
+	pozice				(c. pozice)
+	cekej				()			//ceka dokud se motory hybou
+	uchop				()
+	povol				()
+	poloz				()
+	zvedni				()
+	nasad_robot			()
+	check_hraci_pole	()		//true dokud je neco v POLI ne v domeckach
+
 */
 
 void run()
 {
 	uint8_t kostka_hod;
 	inicializace();
-	//nastav_rychlost();
-
-	/*for(;;){
-		aktualizuj_pozice();
-		
-	}*/
+	nastav_rychlost();	
 	
 	while(true)
 	{
-		aktualizuj_pozice();
+		aktualizuj_hraci_pole();
 		if(buttons.isStart())
 		{
 			kostka_hod = kostka;
-			if(CHECKPOLE());	//nìco je v poli
-			{
-				
+			if(check_hraci_pole())	//neco je v poli
+			{				
 			}
-			else()	//nic není v poli
+					
+			else	//nic neni v poli
 			{
 				if(kostka_hod==6)
 				{
-					if(hodnoty_pole[][])	//je obsazeno
-					{
-						vyhod(8);
-					}
-					nasad();						
+					//if(hodnoty_pole[][])	//je obsazeno
+					//{
+					//	vyhod(8);
+					//}
+					nasad_robot();
 				}
-			}	
+			}
 		}
 	}
 }
--- Clovece, nezlob se/Clovece v2.1 (liberec 2012)/pohyby.h
+++ Clovece, nezlob se/Clovece v2.1 (liberec 2012)/pohyby.h
@@ -5,17 +5,22 @@
  *  Author: jirka1
 */
 
-#include <avr/delay.h>
+#include <util/delay.h>
 
 #define	rychlost_motoru		250
 #define pocet_motoru		6
 #define	speed_koef			0,8
 #define kostka				((TCNT0 % 6)+1)
+#define SETBIT(ADDRESS,BIT) (ADDRESS |= (1<<BIT)) 
+#define CHECKBIT(ADDRESS,BIT) ((ADDRESS & (1<<BIT))!=0) 
+#define CLEARBIT(ADDRESS,BIT) (ADDRESS &= ~(1<<BIT)) 
+#define CHECKPOLICKO(BIT)	CHECKBIT(hraci_pole,BIT)
 
 uint8_t i=0;		//promnìná do smyèek
 uint8_t i2=0;		//-------||----------
+uint32_t hraci_pole=0;
 
-const uint16_t hodnoty_pozice[7][33] PROGMEM =
+const uint16_t hodoty_motoru[pocet_motoru+1][33] PROGMEM =
 {
 //0		1		2		3		4		5		6		7		8		9		10		11		12		13		14		15		16		17		18		19		20		21		22		23		24		25		26		27		28		29		30		31		32
 {512,	50,		14,		587,	554,	510,	468,	428,	1014,	972,	935,	892,	858,	819,	780,	743,	700,	659,	626,	589,	548,	510,	470,	434,	392,	352,	316,	283,	239,	202,	168,	126,	88	},
@@ -29,29 +34,25 @@ const uint16_t hodnoty_pozice[7][33] PROGMEM =
 
 const uint16_t hodnoty_pohyb[3][2] PROGMEM =
 {
-{512,	512};
-{512,	512};
+{512,	512},
+{512,	512},
 {512,	512}
 };
 
 const uint16_t hodnoty_rychlost[7][1] PROGMEM =
 {
-{1023},	//1
+{rychlost_motoru},	//1
 {rychlost_motoru},	//2
 {rychlost_motoru},	//3
-{1023},				//4
+{rychlost_motoru},	//4
 {rychlost_motoru},	//5
 {rychlost_motoru},	//6
 {rychlost_motoru}	//7
 };
 
-bool hodnoty_pole[5][8] = 
-{//1	2		3		4		5		6		7		8
-{false,	false,	false,	false,	false,	false,	false,	false	},	//1
-{false,	false,	false,	false,	false,	false,	false,	false	},	//2
-{false,	false,	false,	false,	false,	false,	false,	false	},	//3
-{false,	false,	false,	false,	false,	false,	false,	false	},	//4
-{false,	false,	false,	false,	false,	false,	false,	false	}	//5
+bool domecek[1][8]
+{
+{false,	false,	false,	false,	false,	false,	false,	false	}	//hodnoty domeckù
 };
 
 void inicializace()
@@ -63,11 +64,10 @@ void cekej()
 {
 	i=1;
 	_delay_ms(150);
-	do 
+	while(i!=pocet_motoru)
 	{
 		if (motor[i].present_speed()<1)	{i++;}
-		//else {i++;}
-	} while (i!=pocet_motoru);
+	} 
 }
 
 void nastav_rychlost()
@@ -82,80 +82,89 @@ void zakladni_pozice()
 {
 	for(i=1; i!=pocet_motoru; i++)
 	{
-		motor[i].position(pgm_read_word(&(hodnoty_pozice[(i-1)][0])));
+		motor[i].position(pgm_read_word(&(hodoty_motoru[(i-1)][0])));
 	}
-cekej();
+	cekej();
 }
 
 void pozice(uint8_t c_pozice)
 {
 	for(i=1; i!=pocet_motoru; i++)
 	{
-		motor[i].position(pgm_read_word(&(hodnoty_pozice[(i-1)][c_pozice])));
+		motor[i].position(pgm_read_word(&(hodoty_motoru[(i-1)][c_pozice])));
 	}
 }
 
-void uchop()
+void uchop()		//otevrit celisti
+{
+	motor[6].position(486);
+	motor[7].position(548);
+	cekej();
+}
+
+void povol()		//zavrit celisti
+{
+	motor[6].position(520);
+	motor[7].position(504);
+	cekej();
+}
+
+void zvedni()
 {
 	
 }
 
 void poloz()
 {
+
+}
+
+void nasad_robot()
+{
 	
 }
 
-void vyhod(uint16_t pos)
+bool check_hraci_pole()
 {
-	pozice(pos);
-	uchop();
-	if(hodnoty_pole[][])	//kde ma hrac volno v domecku
+	bool vysledek=false;
+	for (i=1; i!=32; i++)
 	{
-		pozice();
-		poloz();
+		if(CHECKPOLICKO(i)) {i=31; vysledek=true;}
 	}
-	else if(hodnoty_pole[][])
-	{
-		pozice();
-		poloz();
-	}
+	return vysledek;
 }
-void nasad()
-{
-	if(hodnoty_pole[][])
-	{
-		pozice();
-	}
-	else
-	{
-		pozice();
-	}
-	uchop();
-	pozice(8);
-	poloz();
-	
-}
-void aktualizuj_pozice()
+
+void aktualizuj_hraci_pole()
 {
 	uint8_t temp_pozice = 0;
-	for (i=0; i!=5; i++)
+	for (i=0; i!=4; i++)
+	{
+		temp_pozice  = pc.get(); 
+		pc<<temp_pozice<<endl;
+		for (i2=0; i2!=8; i2++)
+		{
+			if (CHECKBIT(temp_pozice,i2)==1)	{SETBIT  (hraci_pole,i*8+i2);}			
+			if (CHECKBIT(temp_pozice,i2)==0)	{CLEARBIT(hraci_pole,i*8+i2);}			 		
+			pc<<((temp_pozice & (1 << i2)) != 0)<<"_";
+		}
+		pc<<hraci_pole<<endl;
+	}
+	
+	for (i=0; i!=1; i++)
 	{
 		temp_pozice  = pc.get();
 		pc<<temp_pozice<<endl;
 		for (i2=0; i2!=8; i2++)
 		{
-			hodnoty_pole[i][i2] = ((temp_pozice & (1 << i2)) != 0);
+			domecek[i][i2] = ((temp_pozice & (1 << i2)) != 0);
 			pc<<((temp_pozice & (1 << i2)) != 0)<<"_";
 		}
-		pc<<"tabulka:"<<endl;
+		pc<<domecek<<endl<<endl;
 	}
 	
-	for (i=0; i!=5; i++)
+	for (i=0; i!=32; i++)
 	{
-		for (i2=0; i2!=8; i2++)
-		{
-			pc<<hodnoty_pole[i][i2];
-		}
-		pc<endl;
+		pc<<CHECKBIT(hraci_pole,i);
 	}
+	pc<endl;
 }
